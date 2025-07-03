2003290458a966bbe5f7976c3fef0e0c9ca341ff
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-07-04 00:37:15 +0700

    update auth

---
+++ toko-beras-backend/internal/auth/auth.go
@@ -0,0 +1,43 @@
+package auth
+
+import (
+	"time"
+	"toko-beras/backend/config" // Sesuaikan dengan nama modul Anda
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var JwtKey = []byte(config.LoadConfig("JWT_SECRET_KEY"))
+
+type Claims struct {
+	UserID int    `json:"user_id"`
+	Email  string `json:"email"`
+	Role   string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func GenerateJWT(userID int, email, role string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &Claims{
+		UserID: userID,
+		Email:  email,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(JwtKey)
+}
