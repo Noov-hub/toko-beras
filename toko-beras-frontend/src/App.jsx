de76d303b12eb0b18fac414150ba13de0504e8e7
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-07-08 00:55:58 +0700

    login register page update

--- toko-beras-frontend/src/App.jsx
+++ toko-beras-frontend/src/App.jsx
@@ -4,20 +4,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductListPage from './pages/ProductListPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage'; // <-- Tambahkan import ini
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Navbar akan muncul di semua halaman */}
       <Navbar />
-
       <main>
-        {/* Routes akan menentukan komponen mana yang ditampilkan berdasarkan URL */}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/produk" element={<ProductListPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} /> {/* <-- Tambahkan rute ini */}
         </Routes>
       </main>
     </BrowserRouter>
