abfabd3e927b9f117bc36f4a77c322aaddb51747
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-07-09 05:13:21 +0700

    pembaruan halaman keranjang

--- toko-beras-frontend/src/pages/CartPage.jsx
+++ toko-beras-frontend/src/pages/CartPage.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import { useCart } from '../context/CartContext';
 import './CartPage.css';
 
 const API_URL = 'http://localhost:8080';
 
 function CartPage() {
-  const { cartItems } = useCart();
+  const { cartItems, removeFromCart } = useCart();
 
   // Hitung total harga
   const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
@@ -29,13 +30,21 @@ function CartPage() {
               <h4>{item.name}</h4>
               <p>Rp {item.price.toLocaleString('id-ID')}</p>
             </div>
+              <button
+              onClick={() => removeFromCart(item.id)}
+              className="remove-btn"
+            >
+              Hapus
+            </button>
             {/* Nanti kita bisa tambahkan tombol untuk ubah jumlah atau hapus */}
           </li>
         ))}
       </ul>
       <div className="cart-summary">
         <h2>Total: Rp {totalPrice.toLocaleString('id-ID')}</h2>
-        <button className="checkout-btn">Lanjutkan ke Checkout</button>
+        <Link to="/checkout">
+            <button className="checkout-btn">Lanjutkan ke Checkout</button>
+        </Link>
       </div>
     </div>
   );
