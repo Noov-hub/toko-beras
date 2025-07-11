4e243910bf9307ac18a1173326a1bf68af9edaf2
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-07-11 23:28:09 +0700

    tambah buat forward porting

--- toko-beras-frontend/src/api/orderApi.js
+++ toko-beras-frontend/src/api/orderApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/api';
+// const API_URL = 'https://7h81qk4k-8080.asse.devtunnels.ms/api';
 
 export const createOrder = async (orderData, token) => {
   return await axios.post(`${API_URL}/orders`, orderData, {
